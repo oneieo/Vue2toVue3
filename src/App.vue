f7d5893f08c7c84a1fd750efb14efaf6fa203345
oneieo <sun020716@gmail.com>
2025-11-07 03:01:23 +0900

    edit: example5 / vue2 -> vue3

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <E06ParentComponent />
+  <E09CompositionApi2 />
 </template>
 
 <script lang="ts">
@@ -9,12 +9,15 @@ import { defineComponent } from "vue";
 //import E03Binding from "@/components/example1/E-03-binding.vue";
 //import E04Directives from "@/components/example2/E-04-directives.vue";
 //import E05ParentComponent from "@/components/example3/ParentComponent.vue";
-import E06ParentComponent from "@/components/example4/ParentComponent.vue";
+//import E06ParentComponent from "@/components/example4/ParentComponent.vue";
+//import E07OptionsApi from "@/components/example5/E-07-Options-API.vue";
+//import E08CompositionApi from "@/components/example5/E-08-composition-api.vue";
+import E09CompositionApi2 from "@/components/example5/E-09-composition-API2.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    E06ParentComponent,
+    E09CompositionApi2,
   },
 });
 </script>
--- src/components/example5/E-07-Options-API.vue
+++ src/components/example5/E-07-Options-API.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>
     <h2>{{ title }}</h2>
     <p>Full Name: {{ fullName }}</p>
@@ -12,28 +12,28 @@
 
 <script>
 export default {
-  name: 'E07OptionsApi',
+  name: "E07OptionsApi",
 
   props: {
     title: {
       type: String,
-      default: 'User Information'
-    }
+      default: "User Information",
+    },
   },
 
   data() {
     return {
-      firstName: 'John',
-      lastName: 'Doe',
+      firstName: "John",
+      lastName: "Doe",
       greetCount: 0,
-      message: ''
+      message: "",
     };
   },
 
   computed: {
     fullName() {
       return `${this.firstName} ${this.lastName}`;
-    }
+    },
   },
 
   methods: {
@@ -43,7 +43,7 @@ export default {
     },
     resetGreetCount() {
       this.greetCount = 0;
-    }
+    },
   },
 
   watch: {
@@ -52,39 +52,125 @@ export default {
       if (newValue >= 3) {
         this.message = "That's enough greetings for now!";
       }
-    }
+    },
   },
 
   beforeCreate() {
-    console.log('beforeCreate hook');
+    console.log("beforeCreate hook");
   },
 
   created() {
-    console.log('created hook');
+    console.log("created hook");
   },
 
   beforeMount() {
-    console.log('beforeMount hook');
+    console.log("beforeMount hook");
   },
 
   mounted() {
-    console.log('mounted hook');
+    console.log("mounted hook");
   },
 
   beforeUpdate() {
-    console.log('beforeUpdate hook');
+    console.log("beforeUpdate hook");
   },
 
   updated() {
-    console.log('updated hook');
+    console.log("updated hook");
   },
 
   beforeUnmount() {
-    console.log('beforeUnmount hook');
+    console.log("beforeUnmount hook");
   },
 
   unmounted() {
-    console.log('unmounted hook');
-  }
+    console.log("unmounted hook");
+  },
 };
+</script> -->
+
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <p>Full Name: {{ fullName }}</p>
+    <input v-model="firstName" placeholder="First Name" />
+    <input v-model="lastName" placeholder="Last Name" />
+    <button @click="greet">Greet</button>
+    <p>Greeting Count: {{ greetCount }}</p>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import {
+  ref,
+  computed,
+  watch,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  defineProps,
+} from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: "User Information",
+  },
+});
+
+const firstName = ref("John");
+const lastName = ref("Doe");
+const greetCount = ref(0);
+const message = ref("");
+
+const fullName = computed(() => {
+  return `${firstName.value} ${lastName.value}`;
+});
+
+const greet = () => {
+  greetCount.value++;
+  message.value = `Hello, ${fullName.value}!`;
+};
+
+const resetGreetCount = () => {
+  greetCount.value = 0;
+};
+
+watch(greetCount, (newValue, oldValue) => {
+  console.log(`Greet count changed from ${oldValue} to ${newValue}`);
+  if (newValue >= 3) {
+    message.value = "That's enough greetings for now!";
+  }
+});
+
+console.log("beforeCreate hook");
+console.log("created hook");
+
+onBeforeMount(() => {
+  console.log("beforeMount hook");
+});
+
+onMounted(() => {
+  console.log("mounted hook");
+});
+
+onBeforeUpdate(() => {
+  console.log("beforeUpdate hook");
+});
+
+onUpdated(() => {
+  console.log("updated hook");
+});
+
+onBeforeUnmount(() => {
+  console.log("beforeUnmount hook");
+});
+
+onUnmounted(() => {
+  console.log("unmounted hook");
+});
 </script>
